279198448506cce57c90cbd34349fab276d8d691
SherryNinja10 <aarshdeepsinghvandal@gmail.com>
2025-02-11 10:16:40 -0800

    Made the array structure for the patients

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
+int patientID[50];
+char patientName[50][50];
+int patientAge[50];
+char patientDiagnosis[50][50];
+int roomNumber[50];
 
 int main(void)
 {
