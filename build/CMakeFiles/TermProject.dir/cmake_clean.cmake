8f0ead5078860d4ebcb39c712b875b970e4b4be2
AllanNguyen <allandog06@gmail.com>
2025-03-29 14:21:35 -0700

    Almost finished adding a new patient but realloc of memoyr and proper patient adding to array still needs to be done

---
+++ build/CMakeFiles/TermProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TermProject.dir/Phase_2/hospital.c.obj"
+  "TermProject.exe"
+  "TermProject.exe.manifest"
+  "TermProject.lib"
+  "TermProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TermProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
